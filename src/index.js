0f116f6e14176f7a5c39a5bf65080ee026d2bb0f
SubtleTea <ariel.lim.us@gmail.com>
2020-07-30 15:04:29 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { getDefaultNormalizer } from '@testing-library/react';
 
 const App = () => {
     const buttonText = { text: 'Click me'};
     const labelText = 'Enter name:';
+
+    function getTime() {
+        return (new Date()).toLocaleTimeString()
+    }
 
 
     return (
@@ -16,7 +21,33 @@ const App = () => {
                 {buttonText.text}
             </button>
 
+            <div id="child">
+            <h3>{getTime()}</h3>
+            </div>
+
+            <div className="ui container comments">
+                <div className="comment">
+                    <a href="/" className="avatar">
+                        <img alt="avatar" />
+                    </a>
+                    <div className="content">
+                        <a href="/" className="author">
+                            Sam
+                        </a>
+                        <div className="metadata">
+                            <span className="date">
+                                <h3>{getTime()}</h3>
+                            </span>
+                        </div>
+                        <div className="text">
+                            Comment
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
+
     );
 };
 
